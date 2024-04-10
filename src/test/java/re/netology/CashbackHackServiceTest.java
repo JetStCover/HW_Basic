10c61918d2e44693c653bf583e8d0619149f5a99
Dan Kotan <underz1997@gmail.com>
2024-04-10 21:22:33 +0300

    added JUnit4

--- src/test/java/re/netology/CashbackHackServiceTest.java
+++ src/test/java/re/netology/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package re.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
